69c7541c38fe9ce65985b06331bde52a2440dd1b
Jimmy Provencher <jproven3@VDI-LINUX-6.ad.ypg.com>
2020-11-02 16:36:47 -0500

    Push all jimbodragon_chef_repo project

--- initialize/git_push.sh
+++ initialize/git_push.sh
@@ -14,7 +14,7 @@ for github_repo in "${git_repos[@]}"
 do
   cd $main_repo_dir
   eval $github_repo
-  #executing_git_clone $type $name $fork_from_public
+  echo "Pushing $type $name $fork_from_public $git_url"
   cd $type/$name
   commit_and_push "Push all $git_main_project_name project"
   echo
