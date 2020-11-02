0eee4c94eb5cb381ab3d851b86f963c21bfad3a7
Jimmy Provencher <jproven3@VDI-LINUX-6.ad.ypg.com>
2020-11-02 16:53:36 -0500

    renaming cookbooks.sh ro git_repos.sh

--- cookbooks.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cookbooks=(
-'name=infraClass type=libraries'
-'name=infra_chef type=cookbooks'
-'name=virtualbox-install type=cookbooks'
-'name=chef-ingredient type=cookbooks fork_from_public=git@github.com:chef-cookbooks/chef-ingredient.git'
-)
