f5f8985adbb2452ae3c4258120107278e08815e7
Jimmy Provencher <jimmy.provencher@cgi.com>
2020-12-06 15:09:36 -0500

    set install_name

--- functions/initialize.sh
+++ functions/initialize.sh
@@ -180,7 +180,7 @@ function run_project()
   prepare_project
   validate_project is_good
 
-  new_chef_infra "$project_name" "$git_branch" "$environment" "$git_main_project_name" "$git_org" "$git_baseurl" "$git_user" "$http_git" "$initialize_script_name" "$chef_repo_path" "$initial_role" "$initial_workstation_cookbook" "$initial_current_dir" "$default_chef_path" "$is_require_git_clone"
+  new_chef_infra "$project_name" "$git_branch" "$environment" "$git_main_project_name" "$git_org" "$git_baseurl" "$git_user" "$http_git" "$initialize_script_name" "$chef_repo_path" "$initial_role" "$initial_workstation_cookbook" "$initial_current_dir" "$default_chef_path" "$is_require_git_clone" "$install_file_name"
 
   case $is_good in
     0 )
