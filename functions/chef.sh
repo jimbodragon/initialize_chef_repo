5e4361431d0a1daaafae2ae959479601653562b6
Jimmy Provencher <jimmy.provencher@cgi.com>
2020-12-07 19:41:20 -0500

    Adding $initial_workstation_cookbook in default run_list

--- functions/chef.sh
+++ functions/chef.sh
@@ -319,13 +319,13 @@ export -f change_chef_parameter
 
 function ensure_default_attributes
 {
-  echo "\"default_attributes\": {\"$chef_workstation_initialize]\": {\"project_name\": \"$project_name\", \"environments\": [\"$chef_environment\"], \"install_dir\": \"$install_dir\", \"gitinfo\": {}, \"chef_initialized\": true}}"
+  echo "\"default_attributes\": {\"$chef_workstation_initialize]\": {\"project_name\": \"$project_name\", \"environments\": [\"$chef_environment\"], \"install_dir\": \"$initialize_install_dir\", \"gitinfo\": {}, \"chef_initialized\": true}}"
 }
 export -f ensure_default_attributes
 
 function project_json
 {
-  echo "{\"name\": \"$project_name\",\"description\": \"$project_description\",\"chef_type\": \"${1,,}\",\"json_class\": \"Chef::$1\",$(ensure_default_attributes), \"override_attributes\": {},\"run_list\": [\"recipe[$chef_workstation_initialize]\"]}"
+  echo "{\"name\": \"$project_name\",\"description\": \"$project_description\",\"chef_type\": \"${1,,}\",\"json_class\": \"Chef::$1\",$(ensure_default_attributes), \"override_attributes\": {},\"run_list\": [\"recipe[$initial_workstation_cookbook]\"]}"
 }
 export -f project_json
 
