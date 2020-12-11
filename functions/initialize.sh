e7a20c2c2ee9409feaf1bd351d3cce7ec10788bb
Jimmy Provencher <jimmy.provencher@cgi.com>
2020-12-10 21:20:37 -0500

    logging

--- functions/initialize.sh
+++ functions/initialize.sh
@@ -133,6 +133,7 @@ function wait_for_command()
           log "$day day $hour h $min min $sec sec"
           if [ $day -eq $4 ] && [ $hour -eq $3 ] && [ $min -eq $2 ] && [ $sec -eq 0 ] || [ "$first_run$day$hour$min$sec" == "10000" ]
           then
+            log "executing $5"
             eval $(echo -e "$5")
             first_run=0
             return
