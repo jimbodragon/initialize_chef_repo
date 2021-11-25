87d05f240329d4b6945cb6066676f5965272381c
Jimmy Provencher <jimmy.provencher@cgi.com>
2021-11-25 01:18:32 -0500

    Do not remove old directory

--- functions/initialize.sh
+++ functions/initialize.sh
@@ -193,14 +193,14 @@ function clear_project()
 {
   log "Clear Project: $chef_repo_path | $project_name"
   cd $initial_current_dir
-  rm -rf $default_chef_path
+  delete_directory $default_chef_path
   rm install.sh*
-  rm -rf $functions_dir_narm
-  rm -rf $initialize_dir_name
-  rm -rf $build_dir_name
-  rm -rf $data_dir_name
-  rm -rf $log_dir_name
-  rm -rf $install_dir_name
+  delete_directory $functions_dir_name
+  delete_directory $initialize_dir_name
+  delete_directory $build_dir_name
+  delete_directory $data_dir_name
+  delete_directory $log_dir_name
+  delete_directory $install_dir_name
   is_good=$(validate_project)
 
   case $is_good in
@@ -351,7 +351,7 @@ function run_project()
       new_source_file="$new_project_folder/$data_dir_name/$(basename ${BASH_SOURCE[0]})"
       log_bold "Switching to new_source_file '$new_source_file': Old one is '$source_file'"
       copy_project "$new_project_folder"
-      delete_directory_project
+      # delete_directory_project
       initialize_parameters "$new_source_file"
       redefine_data
       rename_project $project_name
